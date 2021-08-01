18c4ded5a12466d1ae365f7120edd4a7bfa2d325
vcyzteen <vcyzscape@gmail.com>
2021-08-01 21:42:58 +0700

    Import ARM GCC 20210801 NON ELF
    Build completed on: Sun Aug  1 21:42:58 WIB 2021
    
    Configuration: /home/runner/work/_temp/3342842e-905f-4475-b4c7-41fcae6b2c40.sh: line 13: ./bin/arm-eabi-gcc: No such file or directory
    
    LLD: LLD 14.0.0 (https://github.com/llvm/llvm-project 7d855605830f4a524f02b09d6891b351ff716782) (compatible with GNU linkers)
    Signed-off-by: vcyzteen <vcyzscape@gmail.com>

---
+++ lib/gcc/arm-linux-gnueabi/12.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=arm-linux-gnueabi --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-lto --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Baka GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
