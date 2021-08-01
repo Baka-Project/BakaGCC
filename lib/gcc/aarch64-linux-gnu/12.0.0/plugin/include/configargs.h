f390a6f36ca2ddfb2259d79e39cf99f5a4b0866f
vcyzteen <vcyzscape@gmail.com>
2021-08-01 21:43:07 +0700

    Import ARM64 GCC 20210801 NON ELF
    Build completed on: Sun Aug  1 21:43:07 WIB 2021
    
    Configuration: /home/runner/work/_temp/10d38e16-55f1-4d6c-8e89-95f3f0fe1572.sh: line 13: ./bin/aarch64-elf-gcc: No such file or directory
    
    LLD: LLD 14.0.0 (https://github.com/llvm/llvm-project 7d855605830f4a524f02b09d6891b351ff716782) (compatible with GNU linkers)
    Signed-off-by: vcyzteen <vcyzscape@gmail.com>

---
+++ lib/gcc/aarch64-linux-gnu/12.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-linux-gnu --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-lto --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Baka GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
