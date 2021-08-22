981656f7c40dafc414c2a08c2655c43562f7eb94
vcyzteen <vcyzscape@gmail.com>
2021-08-22 22:32:51 +0700

    Import ARM GCC 20210822-11-50-12 NON ELF
    Build completed on: Sun Aug 22 22:32:51 WIB 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/arm-linux-gnueabi-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm/bin/../libexec/gcc/arm-linux-gnueabi/12.0.0/lto-wrapper
    Target: arm-linux-gnueabi
    Configured with: ../gcc/configure --target=arm-linux-gnueabi --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Baka GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib
    gcc version 12.0.0 20210822 (experimental) (Baka GCC)
    
    LLD: LLD 14.0.0 (compatible with GNU linkers)
    Signed-off-by: vcyzteen <vcyzscape@gmail.com>

--- lib/gcc/arm-linux-gnueabi/12.0.0/plugin/include/configargs.h
+++ lib/gcc/arm-linux-gnueabi/12.0.0/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=arm-linux-gnueabi --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-lto --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Baka GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char configuration_arguments[] = "../gcc/configure --target=arm-linux-gnueabi --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Baka GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
 static const char thread_model[] = "posix";
 
 static const struct {
