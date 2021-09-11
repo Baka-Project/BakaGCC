fa6d4218bc3377464877731a69f13c385f5bc885
vcyzteen <vcyzscape@gmail.com>
2021-09-11 13:02:12 +0700

    Import ARM64 GCC 20210911-01-06-37 NON ELF
    Build completed on: Sat Sep 11 13:02:12 WIB 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-linux-gnu-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-linux-gnu/12.0.0/lto-wrapper
    Target: aarch64-linux-gnu
    Configured with: ../gcc/configure --target=aarch64-linux-gnu --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Baka GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib
    gcc version 12.0.0 20210911 (experimental) (Baka GCC)
    
    LLD: LLD 14.0.0 (compatible with GNU linkers)
    Signed-off-by: vcyzteen <vcyzscape@gmail.com>

--- lib/gcc/aarch64-linux-gnu/12.0.0/plugin/include/version.h
+++ lib/gcc/aarch64-linux-gnu/12.0.0/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "12.0.0 20210910 (experimental)"
+#define version_string "12.0.0 20210911 (experimental)"
 #define pkgversion_string "(Baka GCC) "
 
 /* This is the location of the online document giving instructions for
